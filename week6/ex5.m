2934498ed43581205712523a07b2cca215582f73
WeizhuoZHANG <weizhuo.zhang@qq.com>
2018-01-26 21:29:19 +0800

    finish the validation curve

--- week6/ex5.m
+++ week6/ex5.m
@@ -104,59 +104,58 @@ lambda = 0;
 %%  Write Up Note: Since the model is underfitting the data, we expect to
 %%                 see a graph with "high bias" -- Figure 3 in ex5.pdf 
 %%
-
-lambda = 0;
-[error_train, error_val] = ...
-    learningCurve([ones(m, 1) X], y, ...
-                  [ones(size(Xval, 1), 1) Xval], yval, ...
-                  lambda);
-
-plot(1:m, error_train, 1:m, error_val);
-title('Learning curve for linear regression')
-legend('Train', 'Cross Validation')
-xlabel('Number of training examples')
-ylabel('Error')
-axis([0 13 0 150])
-
-fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
-for i = 1:m
-    fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
-end
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
+%
+%lambda = 0;
+%[error_train, error_val] = ...
+%    learningCurve([ones(m, 1) X], y, ...
+%                  [ones(size(Xval, 1), 1) Xval], yval, ...
+%                  lambda);
+%
+%plot(1:m, error_train, 1:m, error_val);
+%title('Learning curve for linear regression')
+%legend('Train', 'Cross Validation')
+%xlabel('Number of training examples')
+%ylabel('Error')
+%axis([0 13 0 150])
+%
+%fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
+%for i = 1:m
+%    fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
+%end
+%
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
+%
 %%% =========== Part 6: Feature Mapping for Polynomial Regression =============
 %%  One solution to this is to use polynomial regression. You should now
 %%  complete polyFeatures to map each example into its powers
 %%
 %
-%p = 8;
-%
-%% Map X onto Polynomial Features and Normalize
-%X_poly = polyFeatures(X, p);
-%[X_poly, mu, sigma] = featureNormalize(X_poly);  % Normalize
-%X_poly = [ones(m, 1), X_poly];                   % Add Ones
-%
+p = 8;
+
+% Map X onto Polynomial Features and Normalize
+X_poly = polyFeatures(X, p);
+[X_poly, mu, sigma] = featureNormalize(X_poly);  % Normalize
+X_poly = [ones(m, 1), X_poly];                   % Add Ones
+
 %% Map X_poly_test and normalize (using mu and sigma)
 %X_poly_test = polyFeatures(Xtest, p);
 %X_poly_test = bsxfun(@minus, X_poly_test, mu);
 %X_poly_test = bsxfun(@rdivide, X_poly_test, sigma);
 %X_poly_test = [ones(size(X_poly_test, 1), 1), X_poly_test];         % Add Ones
-%
-%% Map X_poly_val and normalize (using mu and sigma)
-%X_poly_val = polyFeatures(Xval, p);
-%X_poly_val = bsxfun(@minus, X_poly_val, mu);
-%X_poly_val = bsxfun(@rdivide, X_poly_val, sigma);
-%X_poly_val = [ones(size(X_poly_val, 1), 1), X_poly_val];           % Add Ones
+
+% Map X_poly_val and normalize (using mu and sigma)
+X_poly_val = polyFeatures(Xval, p);
+X_poly_val = bsxfun(@minus, X_poly_val, mu);
+X_poly_val = bsxfun(@rdivide, X_poly_val, sigma);
+X_poly_val = [ones(size(X_poly_val, 1), 1), X_poly_val];           % Add Ones
+
 %
 %fprintf('Normalized Training Example 1:\n');
 %fprintf('  %f  \n', X_poly(1, :));
 %
 %fprintf('\nProgram paused. Press enter to continue.\n');
 %pause;
-%
-%
 %
 %%% =========== Part 7: Learning Curve for Polynomial Regression =============
 %%  Now, you will get to experiment with polynomial regression with multiple
@@ -165,7 +164,7 @@ pause;
 %%  lambda to see how the fit and learning curve change.
 %%
 %
-%lambda = 0;
+%lambda = 100;
 %[theta] = trainLinearReg(X_poly, y, lambda);
 %
 %% Plot training data and fit
@@ -201,21 +200,21 @@ pause;
 %%  lambda on a validation set. You will then use this to select the
 %%  "best" lambda value.
 %%
-%
-%[lambda_vec, error_train, error_val] = ...
-%    validationCurve(X_poly, y, X_poly_val, yval);
-%
-%close all;
-%plot(lambda_vec, error_train, lambda_vec, error_val);
-%legend('Train', 'Cross Validation');
-%xlabel('lambda');
-%ylabel('Error');
-%
-%fprintf('lambda\t\tTrain Error\tValidation Error\n');
-%for i = 1:length(lambda_vec)
-%	fprintf(' %f\t%f\t%f\n', ...
-%            lambda_vec(i), error_train(i), error_val(i));
-%end
-%
-%fprintf('Program paused. Press enter to continue.\n');
-%pause;
+
+[lambda_vec, error_train, error_val] = ...
+    validationCurve(X_poly, y, X_poly_val, yval);
+
+close all;
+plot(lambda_vec, error_train, lambda_vec, error_val);
+legend('Train', 'Cross Validation');
+xlabel('lambda');
+ylabel('Error');
+
+fprintf('lambda\t\tTrain Error\tValidation Error\n');
+for i = 1:length(lambda_vec)
+	fprintf(' %f\t%f\t%f\n', ...
+            lambda_vec(i), error_train(i), error_val(i));
+end
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
