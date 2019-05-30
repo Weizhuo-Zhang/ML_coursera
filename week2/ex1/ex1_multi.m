77b5a18c82db8d087ce9e18ce632d04e309f1c86
Weizhuo Zhang <wzhuo.zhang@gmail.com>
2019-05-30 22:52:40 +1000

    finish normal equation

--- week2/ex1/ex1_multi.m
+++ week2/ex1/ex1_multi.m
@@ -104,7 +104,8 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+price = 0;
+price = theta * [1650  3]; % You should change this
 
 
 % ============================================================
@@ -149,8 +150,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
-
+% price = 0;
+price = theta * [1650  3]; % You should change this
 
 % ============================================================
 
